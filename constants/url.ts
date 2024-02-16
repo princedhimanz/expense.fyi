ee0fc790ca732a6081778a9c2e64a72b8c6d473f
Depinder Singh <jandu25@gmail.com>
2024-02-16 22:20:01 +0530

    Update domain in url.ts

--- constants/url.ts
+++ constants/url.ts
@@ -3,7 +3,7 @@ import { views } from './table';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const domain = 'expense.fyi';
+const domain = 'depinder.me';
 const local = 'localhost:3000';
 const home = isProduction ? domain : local;
 
